5442ac4886e54964d08a4d05300f156130576064
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2022-07-27 09:12:18 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # V2Ray new configuration
-cat <<-EOF > /etc/v2ray/config.json
-$CONFIG_JSON
+curl $CONFIG_JSON >> config.json
 EOF
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
